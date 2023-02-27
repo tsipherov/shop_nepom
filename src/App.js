9b83b5c5fb0451d263bb01613c7d47f802fc0d46
tsipherov <tsipherov@gmail.com>
2023-02-27 21:58:35 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fortniteapi.io/v2/shop?lang=en", {
+      method: "GET",
+      headers: {
+        Authorization: "dfbc3e00-cd7be9c4-38dea699-a49c9ade",
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res.shop);
+        setData(res.shop);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
+
+  const filteredData = data.filter((item, ind) => {
+    return ind < 10;
+  });
+
+  const shopGoods = filteredData.map((item, index) => (
+    // const shopGoods = data.map((item, index) => (
+    <h4 key={item.mainId}>{`${index + 1} - ${item.displayName}`}</h4>
+  ));
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>{shopGoods}</ul>
       </header>
     </div>
   );
