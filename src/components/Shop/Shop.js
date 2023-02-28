dcd7423e100610c66a4192fb05cdc1cad96de390
tsipherov <tsipherov@gmail.com>
2023-02-28 23:28:26 +0200

    refactoring incQuantity, decQuantity & addToBasket methods

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -13,7 +13,6 @@ const Shop = () => {
   const [total, setTotal] = useState(0);
 
   useEffect(() => {
-    // setLoading(true);
     fetch(API_URL, {
       method: "GET",
       headers: {
@@ -22,7 +21,6 @@ const Shop = () => {
     })
       .then((res) => res.json())
       .then((res) => {
-        // console.log(res.shop);
         setData(res.shop);
         setLoading(false);
       })
@@ -45,55 +43,54 @@ const Shop = () => {
     return result;
   };
 
-  const filteredData = data.filter((item, ind) => {
-    return ind < 10;
-  });
+  const filteredData = data.filter((item, ind) => ind < 24);
 
   const handleBasketShow = () => {
     setIsBasketShow(!isBasketShow);
   };
 
   const removeFromBasket = (id) => {
-    const changData = order.filter((item) => item.mainId !== id);
-    setOrder(changData);
-    // setTotal(getTotal(changData));
+    const newOrder = order.filter((item) => item.mainId !== id);
+    setOrder(newOrder);
   };
 
   const incQuantity = (id) => {
-    const changData = order.map((item) => {
-      if (item.mainId === id) item.quantity += 1;
+    const newOrder = order.map((item) => {
+      if (item.mainId === id) {
+        return { ...item, quantity: item.quantity + 1 };
+      }
       return item;
     });
-    setOrder(changData);
-    // setTotal(getTotal(changData));
+    setOrder(newOrder);
   };
 
   const decQuantity = (id) => {
-    const changData = order.map((item) => {
-      if (item.mainId === id) item.quantity -= 1;
+    const newOrder = order.map((item) => {
+      if (item.mainId === id) {
+        return { ...item, quantity: item.quantity - 1 };
+      }
       return item;
     });
-    const filteringData = changData.filter((item) => item.quantity !== 0);
-    setOrder(filteringData);
-    // setTotal(getTotal(filteringData));
+    const filteringOrder = newOrder.filter((item) => item.quantity !== 0);
+    setOrder(filteringOrder);
   };
 
   const addToBasket = (product) => {
     let findProduct = order.find((itm) => itm.mainId === product.mainId);
     if (findProduct) {
       const newOrder = order.map((itm) => {
-        if (itm.mainId === product.mainId) itm.quantity = itm.quantity + 1;
+        if (itm.mainId === product.mainId) {
+          return { ...itm, quantity: itm.quantity + 1 };
+        }
         return itm;
       });
       setOrder(newOrder);
-      // setTotal(getTotal(newOrder));
     } else {
       setOrder((order) => [...order, { ...product, quantity: 1 }]);
-      // setTotal(getTotal(order));
     }
   };
 
-  console.log("order >>> ", order);
+  // console.log("order >>> ", order);
   return (
     <main className="container content">
       <Cart quantity={order.length} handleBasketShow={handleBasketShow} />
